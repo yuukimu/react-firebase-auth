28e6aa504bc3a01fbb579031f912e30242e84cca
yuuki <hoge@example.com>
2021-11-07 09:19:19 +0000

    不要な行を削除

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,13 @@
-import { signInWithPopup, GoogleAuthProvider, signOut, User } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, signOut, User, getIdTokenResult } from "firebase/auth";
 import firebaseAuth from './firebase-conf';
 
 export const authAction = () => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(firebaseAuth, provider)
         .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
             const user = result.user;
             return user
         }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.email;
             const credential = GoogleAuthProvider.credentialFromError(error);
             console.error(credential);
             return error;
